ef14638f62847ba991aa076693ccb314a910f6b4
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-05 23:46:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,15 @@ class Artist
      song.artist = self
     end
     
+    def self.find_by_name(name) #class meth 
+      self.all.find {|artist| artist.name == name}
+    end 
+    
     def self.find_or_create_by_name(name)
-      
+      if self.find_by_name(name) #I can find this artist return artist
+      self.find_by_name(name) # return specifi artist
+      else 
+        #create new artist using name 
+        self.new(name) 
     end 
 end 
