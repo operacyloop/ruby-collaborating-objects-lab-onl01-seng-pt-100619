eaf1735a06d933bea835b0d03acaaa74acc0235a
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-06 00:22:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,10 @@ end
 
 def new_by_filename(filename) #class method NOT instance method 
 binding.pry 
+split_files = filename.split(" - ")
+song_name = split_files[1]
+new_song = self.new(song_name) #same as Song.new(song_name) 
+# name part, create an artist, and use artist name that will use other methods
 
 end 
   
